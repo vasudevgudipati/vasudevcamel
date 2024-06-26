c37a852eba0b38f7f7a1754c666b27f9ac4d07d8
DELL <DELL@DESKTOP-NEBOL2J>
2024-06-26 16:33:39 +0530

    Initial Commit

---
+++ cameldemo/src/main/java/com/example/demo/CameldemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CameldemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CameldemoApplication.class, args);
+	}
+
+}
---
+++ cameldemo/src/main/java/com/example/demo/LoggingScheduler.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+@Component
+public class LoggingScheduler extends RouteBuilder {
+
+	@Override
+	public void configure() throws Exception {
+		from("timer:Logtimer?period=10000")
+		.routeId("logging Route")
+		.log("Logging every 10 seconds");
+	}
+
+}
